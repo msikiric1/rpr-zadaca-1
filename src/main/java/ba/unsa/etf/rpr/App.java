18a848738324fc2ccf53fa54eb0c139d4201e728
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-16 23:15:40 +0100

    add temporary test to see if 'evaluate' method is working correctly

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,18 @@
 package ba.unsa.etf.rpr;
 
 /**
- * Hello world!
- *
+ * Zadaca 1
+ * @author Muaz Sikiric
+ * @version 0.1
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    /**
+     * Main method
+     * @param args input arguments from console
+     */
+    public static void main( String[] args ) {
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        double result = evaluator.evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )");
+        System.out.println(result);
     }
 }
