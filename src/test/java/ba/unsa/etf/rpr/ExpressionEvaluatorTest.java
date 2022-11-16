0a515713ab33483ee395a8948a5a86a96b325d8a
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-16 23:19:15 +0100

    rename AppTest to ExpressionEvaluatorTest and fix import errors

---
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for ExpressionEvaluator class
+ */
+public class ExpressionEvaluatorTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
