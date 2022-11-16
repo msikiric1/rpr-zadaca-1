d6c72a073673d18da43caa3ada318d581b4257fa
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-17 00:22:25 +0100

    add test 2

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -1,7 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +12,20 @@ public class ExpressionEvaluatorTest
 {
     ExpressionEvaluator ee = new ExpressionEvaluator();
     /**
-     * Rigorous Test :-)
+     * Test 1
      */
     @Test
     public void evaluate1() {
         String input = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
         assertEquals(101, ee.evaluate(input));
+    }
+
+    /**
+     * Test 2
+     */
+    @Test
+    public void evaluate2() {
+        String input = "( ( ( 1 + 8 ) / 2 ) * ( 2 - ( 0.5 * 7 ) ) )";
+        assertEquals(-6.75, ee.evaluate(input));
     }
 }
