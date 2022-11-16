b94d4a81ae83ef5033f2e2d5b26fecf41c1a6e95
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-16 21:40:48 +0100

    start implementation of ExpressionEvaluator class

---
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Stack;
+
+/**
+ * @author Muaz Sikiric
+ */
+public class ExpressionEvaluator {
+    /**
+     * Evaluates given expression and returns final value if it can be calculated
+     * @param expression
+     * @return value
+     */
+    public Double evaluate(String expression) {
+        Stack<Double> operands = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        return operands.pop();
+    }
+}
