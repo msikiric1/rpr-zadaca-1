a890e1811a1a0f87b1901580fac76c04be4c77e8
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-16 23:27:20 +0100

    add basic test for evaluate method

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -9,12 +10,13 @@ import org.junit.jupiter.api.Test;
  */
 public class ExpressionEvaluatorTest
 {
+    ExpressionEvaluator ee = new ExpressionEvaluator();
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void evaluate1() {
+        String input = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
+        assertEquals(101, ee.evaluate(input));
     }
 }
