287131357e262363f63fdf6dcbae56ebf7c2be82
msikiric1 <msikiric1@etf.unsa.ba>
2022-11-19 09:09:04 +0100

    update test method names

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -14,7 +14,7 @@ public class ExpressionEvaluatorTest
      * Integer expression test
      */
     @Test
-    public void evaluate1() {
+    public void intExpression() {
         String input = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
         assertEquals(101, ee.evaluate(input));
     }
@@ -23,34 +23,25 @@ public class ExpressionEvaluatorTest
      * Integer and double expression test
      */
     @Test
-    public void evaluate2() {
+    public void intAndDoubleExpression() {
         String input = "( ( ( 1 + 8 ) / 2 ) * ( 2 - ( 0.5 * 7 ) ) )";
         assertEquals(-6.75, ee.evaluate(input));
-    }
-
-    /**
-     * Expression inside sqrt operator test
-     */
-    @Test
-    public void evaluate3() {
-        String input = "( sqrt ( ( 18 / 2 ) ) * ( ( 3 + 2 ) - 1 ) )";
-        assertEquals(12, ee.evaluate(input));
     }
 
     /**
      * Double expression test
      */
     @Test
-    public void evaluate4() {
+    public void doubleExpression() {
         String input = "( ( ( 4.25 * 3.8 ) / 4 ) - ( ( 3.5 / 2.5 ) * ( 2.34 - ( 1.2 * 1.2 ) ) ) )";
         assertEquals(2.7775, ee.evaluate(input));
     }
 
     /**
-     * Complex  expression test
+     * Complex expression test
      */
     @Test
-    public void evaluate5() {
+    public void complexExpression() {
         String input = "( ( 5 / ( 100 - 123 ) ) * ( ( ( 6 * ( 5.5 - sqrt ( 225 ) ) ) * 3.14 ) + ( 42 / ( 1234 / sqrt ( ( 64 * 16 ) ) ) ) ) )";
         assertEquals(38.67192586850821, ee.evaluate(input));
     }
@@ -71,6 +62,15 @@ public class ExpressionEvaluatorTest
     public void sqrt() {
         String input = "( ( sqrt ( 49 ) * sqrt ( 121 ) ) / sqrt ( 4 ) )";
         assertEquals(38.5, ee.evaluate(input));
+    }
+
+    /**
+     * Expression inside sqrt operator test
+     */
+    @Test
+    public void expressionInsideSqrt() {
+        String input = "( sqrt ( ( 18 / 2 ) ) * ( ( 3 + 2 ) - 1 ) )";
+        assertEquals(12, ee.evaluate(input));
     }
 
     /**
